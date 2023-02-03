7a221f45b053cec510303cc80d2217f232a4c648
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2023-02-03 14:02:24 +0530

    Stops program once pool is empty.

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,8 @@ pub struct Args {
     path: String,
     #[arg(short, long, default_value_t = 4)]
     threads: usize,
+    #[arg(long)]
+    target_length: Option<usize>,
 }
 
 pub struct Job {
@@ -145,51 +147,63 @@ fn main() -> Result<(), Box<dyn Error>> {
         None
     };
 
-    for res in rx {
-        let payload: String;
-        if res.job.substring == res.job.identifier {
-            let env_crib = format!("${{env:{}}}", &res.expression);
-            payload = path.replace(&res.job.substring, &env_crib);
-        } else {
-            let mut original_value: Option<String> = None;
-            for (value, identifiers) in &environment {
-                if identifiers.iter().any(|i| res.job.identifier.eq(i)) {
-                    original_value = Some(value.clone());
-                    break;
-                }
-            }
-            if let Some(value) = original_value {
-                let begin = value.find(&res.job.substring).unwrap();
+    let mut recv = rx.iter();
 
-                let end = begin + res.job.substring.len() - 1;
-                let env_crib = format!(
-                    "(\"${{env:{}}}\"[{}..{}]-join'')",
-                    &res.expression, begin, end
-                );
-                let mut interpolated = Vec::new();
-                let parts = path.split(&res.job.substring).collect::<Vec<&str>>();
-
-                for (i, part) in parts.iter().enumerate() {
-                    if part.len() == 0 {
-                        if i == 0 {
-                            interpolated.push(env_crib.clone());
-                        }
-                        continue;
-                    }
-                    interpolated.push(format!("\"{}\"", part));
-                    if i == parts.len() - 1 {
-                        continue;
-                    }
-                    interpolated.push(env_crib.clone());
-                }
-                payload = interpolated.join("+");
-            } else {
-                payload = "".to_string();
-            }
+    loop {
+        if pool.active_count() == 0 && pool.queued_count() == 0 {
+            break;
         }
-        println!("{}", payload);
-        if let Some(ref mut f) = handle {
-            writeln!(f, "{}", payload)?;
+
+        if let Some(res) = recv.next() {
+            let payload = if res.job.substring == res.job.identifier {
+                let env_crib = format!("${{env:{}}}", &res.expression);
+                path.replace(&res.job.substring, &env_crib)
+            } else {
+                let mut original_value: Option<String> = None;
+                for (value, identifiers) in &environment {
+                    if identifiers.iter().any(|i| res.job.identifier.eq(i)) {
+                        original_value = Some(value.clone());
+                        break;
+                    }
+                }
+                if let Some(value) = original_value {
+                    let begin = value.find(&res.job.substring).unwrap();
+
+                    let end = begin + res.job.substring.len() - 1;
+                    let env_crib = format!(
+                        "(\"${{env:{}}}\"[{}..{}]-join'')",
+                        &res.expression, begin, end
+                    );
+                    let mut interpolated = Vec::new();
+                    let parts = path.split(&res.job.substring).collect::<Vec<&str>>();
+
+                    for (i, part) in parts.iter().enumerate() {
+                        if part.len() == 0 {
+                            if i == 0 {
+                                interpolated.push(env_crib.clone());
+                            }
+                            continue;
+                        }
+                        interpolated.push(format!("\"{}\"", part));
+                        if i == parts.len() - 1 {
+                            continue;
+                        }
+                        interpolated.push(env_crib.clone());
+                    }
+                    interpolated.join("+")
+                } else {
+                    "".to_string()
+                }
+            };
+            if let Some(length) = args.target_length {
+                if payload.len() > length {
+                    continue;
+                }
+            }
+            println!("{}", payload);
+            if let Some(ref mut f) = handle {
+                writeln!(f, "{}", payload)?;
+            }
         }
     }
     Ok(())
