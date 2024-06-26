b3a3e028995914075c4c1d5436bf7f3fdad7cead
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-06-26 11:14:05 +0530

    feat: use oncelock instead of lazy_static

--- src/syntax.rs
+++ src/syntax.rs
@@ -1,6 +1,5 @@
 use color_eyre::eyre::Result;
-use lazy_static::lazy_static;
-use std::io;
+use std::{io, sync::OnceLock};
 use syntect::{
     easy::HighlightLines,
     highlighting::{Style, Theme, ThemeSet},
@@ -8,30 +7,28 @@ use syntect::{
     util::as_24_bit_terminal_escaped,
 };
 
-lazy_static! {
-    static ref THEME: Theme = {
+static THEME: OnceLock<Theme> = OnceLock::new();
+static SYNTAX: OnceLock<SyntaxSet> = OnceLock::new();
+
+pub fn highlight(s: &str) -> Result<String> {
+    let theme = THEME.get_or_init(|| {
         let theme_data: &'static [u8] = include_bytes!("MonokaiDarkSoda.tmTheme");
         let mut theme_reader: io::Cursor<&[u8]> = io::Cursor::new(theme_data);
         ThemeSet::load_from_reader(&mut theme_reader).unwrap()
-    };
-
-    // This should always result in Some(syntax_reference) since we
-    // have explicitly sourced a PowerShell sublime-syntax file.
-
-    static ref SYNTAX: SyntaxSet = {
-    let syntax: SyntaxDefinition = SyntaxDefinition::load_from_str(
+    });
+    let syntax = SYNTAX.get_or_init(|| {
+        let syntax: SyntaxDefinition = SyntaxDefinition::load_from_str(
             include_str!("PowerShellSyntax.sublime-syntax"),
             false,
             None,
-        ).unwrap();
+        )
+        .unwrap();
         let mut syntax_set_builder = SyntaxSetBuilder::new();
         syntax_set_builder.add(syntax);
         syntax_set_builder.build()
-    };
-}
+    });
 
-pub fn highlight(s: &str) -> Result<String> {
-    let mut h = HighlightLines::new(SYNTAX.find_syntax_by_name("PowerShell").unwrap(), &THEME);
-    let ranges: Vec<(Style, &str)> = h.highlight_line(s, &SYNTAX)?;
+    let mut h = HighlightLines::new(syntax.find_syntax_by_name("PowerShell").unwrap(), theme);
+    let ranges: Vec<(Style, &str)> = h.highlight_line(s, syntax)?;
     Ok(as_24_bit_terminal_escaped(&ranges[..], false))
 }
