88662f0b2c6fe2cd75261d5248b9f2533e656d8e
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-09-26 08:52:25 +0530

    feat: use flakelight for devshell

--- flake.nix
+++ flake.nix
@@ -1,22 +1,14 @@
 {
   description = "devshell for github:lavafroth/envy-rs";
+  inputs = {
+    flakelight.url = "github:nix-community/flakelight";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShells.default = pkgs.mkShell rec {
-            packages = with pkgs;
-            [
-              stdenv.cc.cc.lib
-            ];
-
-            LD_LIBRARY_PATH = "${nixpkgs.lib.makeLibraryPath packages}";
-            LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
-          };
-        }
-      );
+  outputs = { self, flakelight, ... }@inputs:
+    flakelight ./. {
+        inherit inputs;
+        devShells.packages = pkgs: [ pkgs.stdenv.cc ];
+        devShells.env.LD_LIBRARY_PATH = pkgs: "${pkgs.stdenv.cc.cc.lib}/lib";
+    };
 }
